c18b2a572700b4e92ed9058a7ef8c392b9b2095a
TeddyDev <LukeBingham1997@gmail.com>
2017-04-29 16:38:51 +0100

    Fixed Vehicle MPH calculation

--- Gta/src/main/java/me/lukebingham/gta/vehicles/VehicleManager.java
+++ Gta/src/main/java/me/lukebingham/gta/vehicles/VehicleManager.java
@@ -4,11 +4,9 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import me.lukebingham.core.util.Component;
 import me.lukebingham.core.util.PlayerUtil;
-import me.lukebingham.core.util.StringUtil;
 import me.lukebingham.gta.vehicles.type.car.sport.AudiR8;
 import me.lukebingham.util.C;
 import org.bukkit.Location;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerMoveEvent;
 
@@ -39,9 +37,10 @@ public final class VehicleManager implements Component {
         UUID uuid = event.getPlayer().getUniqueId();
         travelDataMap.computeIfAbsent(uuid, key -> new TravelData(System.currentTimeMillis(), event.getTo()));
         TravelData data = travelDataMap.get(uuid);
-        if(System.currentTimeMillis() >= data.getEndTime()) {
+        long now = System.currentTimeMillis();
+        if(now >= data.getEndTime()) {
             double distance = data.getStartLocation().distance(event.getTo());
-            PlayerUtil.sendActionBar(event.getPlayer(), C.YELLOW + "MPH - " + (distance / 0.5));
+            PlayerUtil.sendActionBar(event.getPlayer(), C.YELLOW + "MPH - " + String.format("%.1f", distance / (now - data.getStartTime())));
             travelDataMap.computeIfPresent(uuid, (uuid1, travelData) -> new TravelData(System.currentTimeMillis(), event.getTo()));
         }
     }
